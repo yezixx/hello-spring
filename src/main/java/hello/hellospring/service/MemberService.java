1d0e90d93a6fb05f08e97e787aacddd1bbfa9199
yezixx <skylow917@daum.net>
2023-09-20 22:15:08 +0900

    AOP가 필요한 상황 if 시간 측정이 필요할 떄 -> 로직마다 시간 체크함 -> 유지보수가 어려움 -> 다음 강의에서 해결

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,11 +23,21 @@ public class MemberService {
     * 회원가입
     * */
     public Long join(Member member){
-        // 같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member); // 중복 회원 검증
 
-        memberRepository.save(member);
-        return member.getId();
+        Long start = System.currentTimeMillis();
+
+        try {
+
+            // 같은 이름이 있는 중복 회원x
+            validateDuplicateMember(member); // 중복 회원 검증
+
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            Long finish = System.currentTimeMillis();
+            long timeMs=finish-start;
+            System.out.println("join "+ timeMs+"ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -41,7 +51,16 @@ public class MemberService {
     * 전체 회원 조회
     * */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        Long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        }finally {
+            Long finish = System.currentTimeMillis();
+            long timeMs=finish-start;
+            System.out.println("findMembers "+ timeMs+"ms");
+
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
