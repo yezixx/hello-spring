635eaf7d208a058064f2b0ac02a9a19080c998ae
yezixx <skylow917@daum.net>
2023-09-22 22:23:42 +0900

    AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,20 +24,11 @@ public class MemberService {
     * */
     public Long join(Member member){
 
-        Long start = System.currentTimeMillis();
-
-        try {
-
             // 같은 이름이 있는 중복 회원x
             validateDuplicateMember(member); // 중복 회원 검증
 
             memberRepository.save(member);
             return member.getId();
-        }finally {
-            Long finish = System.currentTimeMillis();
-            long timeMs=finish-start;
-            System.out.println("join "+ timeMs+"ms");
-        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -51,16 +42,9 @@ public class MemberService {
     * 전체 회원 조회
     * */
     public List<Member> findMembers(){
-        Long start = System.currentTimeMillis();
 
-        try {
             return memberRepository.findAll();
-        }finally {
-            Long finish = System.currentTimeMillis();
-            long timeMs=finish-start;
-            System.out.println("findMembers "+ timeMs+"ms");
 
-        }
     }
 
     public Optional<Member> findOne(Long memberId){
